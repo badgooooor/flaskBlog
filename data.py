ee59b649823541db3f349341caaf6b43cd843314
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2017-12-30 19:57:51 +0700

    First commit

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'name': 'Article 1',
+            'body': 'lorem ipsum',
+            'author': 'Brad',
+            'create_date': '12-45-1255'
+        },
+        {
+            'id': 2,
+            'name': 'Article 2',
+            'body': 'lorem ipsum',
+            'author': 'Brad',
+            'create_date': '12-23-1295'
+        },
+        {
+            'id': 3,
+            'name': 'Article 3',
+            'body': 'lorem ipsum',
+            'author': 'Brad',
+            'create_date': '12-17-1855'
+        }
+    ]
+    return articles
