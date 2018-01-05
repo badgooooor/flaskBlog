d38ab29f75ae32aca6cb1737e99ac8826230ef13
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2018-01-05 16:39:04 +0700

    Added user registeration.(Using flask-mysqldb, wtforms and passlib)

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
 from data import Articles
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+
 app = Flask(__name__)
+
+#Config MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'admin'
+app.config['MYSQL_DB'] = 'flaskApp'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+#Initialize MySQL
+mysql = MySQL(app)
+
 Articles = Articles()
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -14,6 +30,47 @@ def about():
 def articles():
     return render_template('articles.html', articles = Articles)
 
+@app.route('/article/<string:id>')
+def article(id):
+    return render_template('article.html',id=id)
+
+# Register form (using WTForms library)
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max =40)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Password do not match.') # Validator check if password is match with confirm field.
+    ])
+    confirm = PasswordField('Confirm password')
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    # make request
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        # accuire data from form in register.html
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        # create cursor and insert data.
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+
+        # commit to db.
+        mysql.connection.commit()
+
+        # close connection
+        cur.close()
+
+        flash('You are now registered and can log in.', 'success')
+
+        redirect(url_for('index'))
+    return render_template('register.html',form=form)
 
 if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
     app.run(debug=True)
